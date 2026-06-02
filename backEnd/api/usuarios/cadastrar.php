5bb9faa5b4880974aefc8f748cc966619704a472
fabasCoder <fabricialvez2@gmail.com>
2026-06-01 21:19:55 -0300

    bug resolvido!

--- backEnd/api/usuarios/cadastrar.php
+++ backEnd/api/usuarios/cadastrar.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-include '../config/conexao.php';
+include '../../config/conexao.php';
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
