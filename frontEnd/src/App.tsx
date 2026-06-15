4f1a1e15ffc349a08ebb4ce8467278bb01ca6256
Theuz <matheusbfcss1@gmail.com>
2026-06-15 15:21:25 -0300

    deletei o teste

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -12,7 +12,8 @@ import CadastroUser from "./components/CadastroUser.tsx";
 import LoginUser from "./components/LoginUser.tsx";
 
 //teste
-//teste
+
+
   function useReveal() {
   const ref = useRef<HTMLDivElement>(null);
   
