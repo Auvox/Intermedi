65e1d7e15cbeee7921ad056a931e3ec6610e457c
Theuz <matheusbfcss1@gmail.com>
2026-06-15 15:20:16 -0300

    commit inical

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -12,6 +12,7 @@ import CadastroUser from "./components/CadastroUser.tsx";
 import LoginUser from "./components/LoginUser.tsx";
 
 //teste
+//teste
   function useReveal() {
   const ref = useRef<HTMLDivElement>(null);
   
