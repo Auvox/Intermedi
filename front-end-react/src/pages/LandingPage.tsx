bacd85fc64ed8c511d3ac8e162733625a7bf129e
fabascoder <fabricialvez2@gmail.com>
2026-06-23 14:21:40 -0300

    Ta chovendo, commit ganrantido

--- front-end-react/src/pages/LandingPage.tsx
+++ front-end-react/src/pages/LandingPage.tsx
@@ -1,12 +1,12 @@
 
 import { useState, useEffect, useRef } from "react";
-import "./LandingPage.css";
-import logoIntermedi from "./assets/logoIntermedi.png";
-import pilula from "./assets/pilula.png";
-import mancha from "./assets/mancha.png";
-import fundoCruz from "./assets/fundoCruz.png"
-import logoI from "./assets/logoI.png"
-import logoFooter from "./assets/logoFooter.png"
+import "../styles/LandingPage.css";
+import logoIntermedi from "../assets/logoIntermedi.png";
+import pilula from "../assets/pilula.png";
+import mancha from "../assets/mancha.png";
+import fundoCruz from "../assets/fundoCruz.png"
+import logoI from "../assets/logoI.png"
+import logoFooter from "../assets/logoFooter.png"
 import MapSection from "../components/sections/MapSection.jsx";
 
 //teste
